80bd43296abf7166c7101cac01b3035081f4c693
sleekcrm1 <sleekcrm@gmail.com>
2026-09-16 02:24:27 +0300

    Change output type to 'export' in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
 const withNextIntl = require('next-intl/plugin')('./config/i18n.ts');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  // ... your other config parameters
-};
-module.exports = nextConfig;
-module.exports = withNextIntl({
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       { hostname: 'www-static.capoeirabelgique.be' },
       { hostname: 'www.capoeirabelgique.be' },
     ],
   },
-});
+};
 
+module.exports = withNextIntl(nextConfig);
